92cd2aeffc59f7ae98ce35fc0cb0e120a621f4ef
marcelomrad <marcelomrad@gmail.com>
2023-02-17 18:14:57 -0300

    feat: inicio do projeto

---
+++ lambda/products/productsFetchFunction.ts
@@ -0,0 +1,28 @@
+
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+
+export async function handler(event: APIGatewayProxyEvent, context: Context) : Promise<APIGatewayProxyResult> {
+
+    const method = event.httpMethod;
+
+    if(event.resource === "/products"){
+        if(method === 'GET'){
+            console.log("GET Products - Success")
+
+            return {
+                statusCode: 200,
+                body: JSON.stringify({
+                    message: "GET Products - Success"
+                })
+            }
+        }
+    }
+
+    return {
+        statusCode: 400,
+        body: JSON.stringify({
+            message: "Bad Request"
+        })
+    }
+
+}
